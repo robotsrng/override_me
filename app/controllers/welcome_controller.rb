f7e3fa1ec643832bf5baa4bd49840e6a0084da80
srng <shaneryanglass88@gmail.com>
2019-03-25 22:05:24 +1000

    Second Look
    After re evaluating the instructions I believe that the requirements are to use internationalization to display the label text, in whatever locale, while
    simultaneously allowing dynamic changes to the Cronut Widget. I think.
    
    As such, I'll be adding a new French locale, in addition to utilizing i18n to swap out the query strings in the label.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
   def index
   end
 end
