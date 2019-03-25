5c16b3910e5d8a499cec19489cf854c1c1ea8049
srng <shaneryanglass88@gmail.com>
2019-03-26 07:03:55 +1000

    Tests in progress
    Tests are up and coming.
    -Added a fix for what was causing some rspec issues into the rails helper
    -Locale is translating perfectly
    -URLS are Dynamic thanks to some string interpolation

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rspec'
+require 'rails_helper'
+require 'support/render_views'
+
+
+RSpec.describe WelcomeController do
+  describe 'GET index' do
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+
+    it 'responds with the proper response status' do
+      get :index
+      expect(response.status).to eq(200)
+    end
+
+    context 'when the user has set their locale to :en' do
+      it 'displays a translated widget to the user' do
+        visit welcome_index_path(locale: :en)
+        expect(page).to have_content(I18n.t(:cronuts_label_html)[0..25])
+        # expect(response.body).to match I18n.t(:cronuts_label_html)
+      end
+    end
+
+    context 'when the user has set their locale to :fr' do
+      it 'displays a translated widget to the user' do
+        visit welcome_index_path(locale: :fr)
+        expect(page).to have_content(I18n.t(:cronuts_label_html)[0..25])
+      end
+
+      it 'does not display english' do
+        visit welcome_index_path(locale: :fr)
+        expect(page).to_not have_content(I18n.t(:cronuts_label_html, locale: :en)[0..25])
+      end
+
+    end
+
+  end
+
+end
